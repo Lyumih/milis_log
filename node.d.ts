e0c2ccb375b95d87ceaa96b1219c447c9c15abc0
Lyumih <Lyumih@users.noreply.github.com>
2025-12-10 07:30:47 +0000

    Deploying to gh-pages from @ Lyumih/milis_log@3abba940cb90727b277287a259d446b38854e46e 🚀

--- node.d.ts
+++ node.d.ts
@@ -120,6 +120,7 @@ declare namespace $ {
         static toJSON(): any;
         destructor(): void;
         static destructor(): void;
+        [Symbol.dispose](): void;
         toString(): string;
     }
 }
@@ -980,6 +981,7 @@ declare namespace $ {
         }): number;
         read(): Uint8Array<ArrayBuffer>;
         truncate(size: number): void;
+        flush(): void;
         close(): void;
         destructor(): void;
     }
@@ -996,6 +998,7 @@ declare namespace $ {
         }): number;
         truncate(size: number): void;
         read(): Uint8Array<ArrayBuffer>;
+        flush(): void;
         close(): void;
     }
 }
