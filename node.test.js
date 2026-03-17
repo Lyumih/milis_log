c93b20f4d62b24323219e5784f6506da4a345197
Lyumih <Lyumih@users.noreply.github.com>
2026-03-17 07:58:56 +0000

    Deploying to gh-pages from @ Lyumih/milis_log@3abba940cb90727b277287a259d446b38854e46e 🚀

--- node.test.js
+++ node.test.js
@@ -7823,6 +7823,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
