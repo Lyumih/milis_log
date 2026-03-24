b7f265df1e26b41742523db2ec298a77093df6cc
Lyumih <Lyumih@users.noreply.github.com>
2026-03-24 07:58:59 +0000

    Deploying to gh-pages from @ Lyumih/milis_log@3abba940cb90727b277287a259d446b38854e46e 🚀

--- node.test.js
+++ node.test.js
@@ -5376,6 +5376,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
--- web.js
+++ web.js
@@ -4148,6 +4148,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
